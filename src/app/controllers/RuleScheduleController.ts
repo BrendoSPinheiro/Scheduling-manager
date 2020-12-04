f06351e96555ccef89aabb2c2bd56f8f7731b60a
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-12-04 12:18:39 -0300

    feat: add list rules method in ruleScheduleController

--- src/app/controllers/RuleScheduleController.ts
+++ src/app/controllers/RuleScheduleController.ts
@@ -1,6 +1,13 @@
-class RuleScheduleController {
-  index() {
+import { Request, Response } from 'express';
+import ListRulesService from '../services/ListRulesService';
 
+class RuleScheduleController {
+  index(request: Request, response: Response) {
+    const listRules = new ListRulesService();
+
+    const rules = listRules.execute();
+
+    return response.status(200).json(rules);
   }
 }
 
