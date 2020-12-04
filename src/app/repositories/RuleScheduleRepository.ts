c685fbd14b90b2631774725cb057dcf5db39e44a
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-12-04 12:05:02 -0300

    feat: create findAll method in rule repository

--- src/app/repositories/RuleScheduleRepository.ts
+++ src/app/repositories/RuleScheduleRepository.ts
@@ -1,7 +1,11 @@
 import Database from '../../database/connection';
 
 class RuleScheduleRepository {
+  findAll() {
+    const rules = Database.readFile();
 
+    return rules;
+  }
 }
 
 export default new RuleScheduleRepository();
