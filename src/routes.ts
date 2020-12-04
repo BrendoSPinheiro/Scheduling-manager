4bf25d607348257d942524ceb51fcc0306b57b83
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-12-04 12:21:24 -0300

    feat: add list rules rote

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
 
+import RuleScheduleController from './app/controllers/RuleScheduleController';
+
 const router = Router();
+
+router.get('/rules', RuleScheduleController.index);
 
 export default router;
