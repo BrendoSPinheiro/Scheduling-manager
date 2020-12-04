c1fc449cd2da9323722aa96bd40c088f31dc1dea
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-12-04 16:49:53 -0300

    fix: update schedule model

--- src/app/models/ScheduleRule.ts
+++ src/app/models/ScheduleRule.ts
@@ -5,14 +5,17 @@ import Interval from './Interval';
 class ScheduleRule {
   id: string;
 
+  type: string;
+
   date?: Date;
 
   weekDays?: string[];
 
   timeInterval: Interval[];
 
-  constructor(date: Date, weekDays: string[], timeInterval: Interval[]) {
+  constructor(type: string, date: Date, weekDays: string[], timeInterval: Interval[]) {
     this.id = uuid();
+    this.type = type;
     this.date = date;
     this.weekDays = weekDays;
     this.timeInterval = timeInterval;
