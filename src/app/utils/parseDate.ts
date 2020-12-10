1361ac60d762f2b47143dc0d790072b27d522815
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-12-10 17:47:05 -0300

    feat: update postman collection

--- src/app/utils/parseDate.ts
+++
@@ -1,9 +0,0 @@
-const zeroFill = (n: number) => (n < 9 ? `0${n}` : `${n}`);
-
-export const parseDate = (date: Date) => {
-  const day = zeroFill(date.getDate() + 1);
-  const month = zeroFill(date.getMonth() + 1);
-  const year = zeroFill(date.getFullYear());
-
-  return `${day}-${month}-${year}`;
-};
